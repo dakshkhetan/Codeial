60b782f58608b8f8934d0f6901bb2832939004b2
Daksh Khetan <2012daksh@gmail.com>
2020-05-02 17:02:38 +0530

    add another controller & router for users/profile

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ const homeController = require('../controllers/home_controller');
 console.log("Router loaded!");
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+// for any further routes, access from here:
+// router.use('/routerName', require('./router_file'));
 
 module.exports = router; 
