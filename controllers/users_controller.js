fb3f064a4777fb9042b4de4513adbdd2f94d1c6d
Daksh Khetan <2012daksh@gmail.com>
2020-05-03 12:58:45 +0530

    add home.ejs and user_profile.ejs

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,8 @@
 module.exports.profile = function(req, res){
-  return res.end('<h1> User Profile </h1>');
-}; 
+  // return res.end('<h1> User Profile page! </h1>');
+  return res.render('user_profile', {
+    title: "User Profile"
+  });
+};
+
+// module.exports.actionName = function(req, res){};
