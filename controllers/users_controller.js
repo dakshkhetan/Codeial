bcf8f4361b4e40100fa3e39ea18a6e4952b1f2f5
Daksh Khetan <2012daksh@gmail.com>
2020-05-05 00:31:09 +0530

    render sign-up & sign-in pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,22 @@
+// module.exports.actionName = function(req, res){};
+
 module.exports.profile = function(req, res){
   // return res.end('<h1> User Profile page! </h1>');
   return res.render('user_profile', {
-    title: "User Profile"
+      title: "User Profile"
   });
 };
 
-// module.exports.actionName = function(req, res){};
+// render the sign up page
+module.exports.signUp = function(req, res){
+  return res.render('user_sign_up', {
+      title: "Codeial | Sign Up"
+  });
+};
+
+// render the sign in page
+module.exports.signIn = function(req, res){
+  return res.render('user_sign_in', {
+      title: "Codeial | Sign In"
+  });
+};
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 const usersController = require('../controllers/users_controller');
 
-router.get('/profile', usersController.profile);
 // router.use('/', require('./posts'));
 
-module.exports = router; 
+router.get('/profile', usersController.profile);
+
+router.get('/sign-in', usersController.signIn);
+router.get('/sign-up', usersController.signUp);
+
+module.exports = router;
