9795073fd5facf83ea711fd501be1e0243e1d69b
Daksh Khetan <2012daksh@gmail.com>
2020-05-06 00:18:52 +0530

    add functionality to sign-up page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,32 +1,54 @@
+const User = require('../models/user');
+
 // module.exports.actionName = function(req, res){};
 
 module.exports.profile = function(req, res){
-  // return res.end('<h1> User Profile page! </h1>');
-  return res.render('user_profile', {
-      title: "User Profile"
-  });
+    // return res.end('<h1> User Profile page! </h1>');
+    return res.render('user_profile', {
+        title: "User Profile"
+    });
 };
 
 // render the sign up page
 module.exports.signUp = function(req, res){
-  return res.render('user_sign_up', {
-      title: "Codeial | Sign Up"
-  });
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
+    });
 };
 
 // render the sign in page
 module.exports.signIn = function(req, res){
-  return res.render('user_sign_in', {
-      title: "Codeial | Sign In"
-  });
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    });
 };
 
 // get the sign up data
 module.exports.create = function(req, res){
-  // TODO Later
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    // find user by 'email' parameter/property
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('Error in finding user in signing up.');
+            return;
+        }
+        if(!user){  // if user is not created
+            User.create(req.body, function(err, user){
+                if(err){
+                    console.log('Error in creating user while signing up.');
+                    return;
+                }
+                return res.redirect('/users/sign-in');    //url
+            })
+        } else {    // i.e. user is already created
+            return res.redirect('back');
+        }
+    })
 };
 
 // sign in and create a session for user
 module.exports.createSession = function(req, res){
-  // TODO Later
+    // TODO Later
 };
--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,6 @@ router.get('/profile', usersController.profile);
 router.get('/sign-in', usersController.signIn);
 router.get('/sign-up', usersController.signUp);
 
+router.post('/create', usersController.create);
+
 module.exports = router;
