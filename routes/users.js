c341208ddb6a34b42e570f70e1079b4feeb5f33f
Daksh Khetan <2012daksh@gmail.com>
2020-05-14 22:51:27 +0530

    set up current authenticated user

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const usersController = require('../controllers/users_controller');
 
 // router.use('/', require('./posts'));
 
-router.get('/profile', usersController.profile);
+router.get('/profile', passport.checkAuthentication, usersController.profile);
 
 router.get('/sign-in', usersController.signIn);
 router.get('/sign-up', usersController.signUp);
