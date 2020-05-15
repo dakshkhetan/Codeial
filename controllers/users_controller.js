0ad41916842bfc17ff505c1559b9fcfd5de378fe
Daksh Khetan <2012daksh@gmail.com>
2020-05-15 22:56:30 +0530

    pass user data to views & limit page access

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -11,6 +11,11 @@ module.exports.profile = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     });
@@ -18,6 +23,11 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     });
