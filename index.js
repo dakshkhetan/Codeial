2af7df3215ceec7cdc62d33a715310d5af61216a
Daksh Khetan <2012daksh@gmail.com>
2020-05-22 19:39:50 +0530

    set up SASS

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const session = require('express-session');   // used for session cookie
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
+
+// SASS middleware
+app.use(sassMiddleware({
+    src: '/assets/scss',
+    dest: '/assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
 
 // helps in reading through the POST request
 app.use(express.urlencoded());
