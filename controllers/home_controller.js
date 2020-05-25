db8152b93d50a644b84eb8e68a132aa3a32e5af1
Daksh Khetan <2012daksh@gmail.com>
2020-05-25 22:09:06 +0530

    display name of the user (below posts)

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -7,8 +7,17 @@ module.exports.home = function(req, res){
 
     // return res.end('<h1> Express is up for Codeial! </h1>');
 
-    // this query will return all the posts
-    Post.find({}, function(err, posts){
+    // // this query will return all the posts
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // this query will return all the posts and
+    // populate the user of each post (i.e. fetching user Object from user ObjectId)
+    Post.find({}).populate('user').exec(function(err, posts){
         return res.render('home', {
             title: "Codeial | Home",
             posts: posts
