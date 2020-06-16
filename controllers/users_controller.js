e103c19d9ba2e16cf1a0433dd89f74a3d89ec422
Daksh Khetan <2012daksh@gmail.com>
2020-06-16 22:24:17 +0530

    create flash messages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -77,11 +77,13 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for user
 module.exports.createSession = function(req, res){
+    req.flash('success', "Logged in successfully.");
     return res.redirect('/');
 };
 
 // sign out
 module.exports.destorySession = function(req, res){
     req.logout();
+    req.flash('success', "You have logged out!");
     return res.redirect('/');
 };
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
 
 // SASS middleware
 app.use(sassMiddleware({
@@ -63,6 +65,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(customMware.setFlash);
 
 // use express router
 app.use('/', require('./routes'));
