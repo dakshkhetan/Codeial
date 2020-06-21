0844755ebb154ec5d4f54e67aae232500ba32153
Daksh Khetan <2012daksh@gmail.com>
2020-06-21 22:20:05 +0530

    create a post :: receiving data

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,7 +1,10 @@
 {
+  // this is done to prevent the page from reloading again & again 
+  // after creation of every post, i.e. we need to convert it to AJAX
+
   // method to submit the form data for new post using AJAX
   let createPost = function(){
-
+      
       let newPostForm = $('#new-post-form');
 
       newPostForm.submit(function(e){
@@ -12,12 +15,53 @@
               url: '/posts/create',
               data: newPostForm.serialize(),
               success: function(data){
-                  console.log(data);
+                  // console.log(data);
+                  let newPost = newPostDom(data.data.post);
+                  $('#posts-list-container>ul').prepend(newPost);
               }, error: function(error){
                   console.log(error.responseText);
               }
           });
       });
+  };
+
+  // method to create a post in DOM
+  let newPostDom = function(post){
+
+      // code copied from _post.ejs in views
+      // then made changes to convert EJS syntax to JQuery
+      // used back-ticks
+      return $(`<li id="post-${ post._id }">
+                  <p>
+                      <small>
+                          <a class="delete-post-button" href="/posts/destroy/${ post.id }">
+                              X
+                          </a>
+                      </small>
+              
+                      ${ post.content }
+                      <br>
+              
+                      <small>
+                          ${ post.user.name }
+                      </small>
+              
+                      <div class="post-comments">
+                          <form action="/comments/create" id="new-comment-form" method="POST">
+                              <input type="text" name="content" placeholder="Type here to add comment..." required>
+                              <input type="hidden" name="post" value="${ post._id }">
+                              <input type="submit" value="Add Comment">
+                          </form>
+              
+                          <div class="post-comments-list">
+                              <ul id="post-comments-${ post._id }">
+                                  
+                              </ul>
+                          </div>
+              
+                      </div>
+                  </p>
+              </li>`);
   };
 
   createPost();
