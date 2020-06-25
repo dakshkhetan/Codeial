9bac97fcedd46fbf574cd8dffcb877a9742b2a55
Daksh Khetan <2012daksh@gmail.com>
2020-06-25 17:33:11 +0530

    install 'postman' & setup API directory structure

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,8 @@ router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comments'));
 
+router.use('/api', require('./api'));
+
 // for any further routes, access from here:
 // router.use('/routerName', require('./router_file'));
 
