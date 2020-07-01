84df8ebb580f115486de792573b224e3452277d7
Daksh Khetan <2012daksh@gmail.com>
2020-07-01 06:20:15 +0530

    create actions for posts API

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const postsApi = require('../../../controllers/api/v1/posts_api');
 
 router.get('/', postsApi.index);
+router.delete('/:id', postsApi.destroy);
 
 module.exports = router;
