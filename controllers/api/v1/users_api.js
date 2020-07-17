bf5b80ad3dcfd87797c6ea185d4ae3fb49da829e
Daksh Khetan <2012daksh@gmail.com>
2020-07-17 22:45:25 +0530

    create jwt token

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../../models/user');
+
+module.exports.createSession = async function(req, res){
+    try {
+
+        let user = await User.findOne({email: req.body.email});
+
+        if(!user || user.password != req.body.password){
+            return res.json(422, {  // 422 : Unprocessable Entity
+                message: "Invalid username/password"
+            });
+        }
+
+        return res.json(200, {
+            message: "Sign-in successful, here is your token, please keep it safe!",
+            data: {
+                // generating token (which contain 'user' info) with encryption key 'codeial'
+                token: jwt.sign(user.toJSON(), 'codeial', {expiresIn: '10000'})
+            }
+        });
+
+    } catch(err) {
+        console.log('Error: ', err);
+        return res.json(500, {
+            message: "Interval Server Error"
+        });
+    }
+};
