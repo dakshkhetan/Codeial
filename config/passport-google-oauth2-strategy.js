52d291dfa43a89953dad7c613e7d7920c1a8a798
Daksh Khetan <2012daksh@gmail.com>
2020-07-20 16:38:19 +0530

    set up passport google-oauth2 strategy

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,43 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+
+// tell passport to use a new strategy for google login
+passport.use(new googleStrategy({
+        // from oauth2.0 key generated on 'console.developers.google.com'
+        clientID: "448487103035-m3assm4hqjfgmss47jgvjbl230pgmfgq.apps.googleusercontent.com",
+        clientSecret: "QOxXTH3qDiGuBWpyk3vQnfHV",
+        callbackURL: "http://localhost:8000/users/auth/google"
+    }, function(accessToken, refreshToken, profile, done){
+        // find a user
+        User.findOne({email: profile.emails[0].value}).exec(function(err, user){
+            if(err){
+                console.log("Error in google-strategy-passport", err);
+                return;
+            }
+
+            console.log(profile);
+
+            if(user){
+                // if found, set this user as req.user
+                return done(null, user);
+            } else {
+                // if not found, create the user and set it as req.user
+                User.create({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    password: crypto.randomBytes(20).toString('hex')
+                }, function(err, user){
+                    if(err){
+                        console.log("Error in creating user :: google-strategy-passport", err);
+                        return;
+                    }
+                    return done(null, user);
+                });
+            }
+        });
+    }
+));
+
+module.exports = passport;
