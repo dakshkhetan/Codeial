72d60e3faa43f44125eb890588ee8efbd89d4698
Daksh Khetan <2012daksh@gmail.com>
2020-07-28 12:43:33 +0530

    setup google-sign-in route & add in views

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const session = require('express-session');   // used for session cookie
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
