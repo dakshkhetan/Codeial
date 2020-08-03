24ff72cd1c498ef986e42e1796c601b3b486c01f
Daksh Khetan <2012daksh@gmail.com>
2020-08-03 22:36:42 +0530

    create template email for adding comment

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -3,14 +3,16 @@ const nodeMailer = require('../config/nodemailer');
 // this is another way of exporting a method
 exports.newComment = (comment) => {
 
-    console.log("** Inside newComment mailer **");
-    console.log("Comment Details:", comment);
+    // console.log("** Inside newComment mailer **");
+    // console.log("Comment Details:", comment);
+
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
         from: 'dakshkhetan@codeial.com',
         to: comment.user.email,
         subject: 'New Comment Published!',
-        html: '<h1>Yup, your comment is now published!</h1>'
+        html: htmlString
     }, (err, info) => {
         if(err){
             console.log("Error in sending mail", err);
