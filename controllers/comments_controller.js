df932544f9d7b7d140a8ed37d03f4b984dff0094
Daksh Khetan <2012daksh@gmail.com>
2020-08-08 13:11:47 +0530

    setup & use kue (comment email worker)

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,8 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
 
 module.exports.create = async function(req, res){
     try {
@@ -23,7 +25,15 @@ module.exports.create = async function(req, res){
             // populating 'user' (with 'name' & 'email' keys) in 'comment' everytime a new comment is created
             comment = await comment.populate('user', 'name email').execPopulate();
             
-            commentsMailer.newComment(comment);
+            // commentsMailer.newComment(comment);
+
+            let job = queue.create('emails', comment).save(function(err){
+                if(err){
+                    console.log("Error in sending to the queue", err);
+                    return;
+                }
+                console.log("Job enqueued:", job.id);
+            });
 
             if(req.xhr){
     
---
+++ workers/comment_email_worker.js
@@ -0,0 +1,10 @@
+const queue = require('../config/kue');
+const commentsMailers = require('../mailers/comments_mailer');
+
+queue.process('emails', function(job, done){
+    console.log("email worker is processing a job", job.data);
+
+    commentsMailers.newComment(job.data);
+
+    done();
+});
