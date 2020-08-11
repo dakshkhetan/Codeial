18369b7b00f32fc40a146d3de65be6519e06ee78
Daksh Khetan <2012daksh@gmail.com>
2020-08-11 18:05:24 +0530

    create Friendship schema

---
+++ models/friendship.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const friendshipSchema = new mongoose.Schema({
+    // the user who sent this request
+    from_user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    // the user who accepted this request
+    to_user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+},{
+    timestamps: true
+});
+
+const Friendship = mongoose.model('Friendship', friendshipSchema);
+module.exports = Friendship;
--- models/user.js
+++ models/user.js
@@ -20,7 +20,13 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String
-    }
+    },
+    friendships: [
+        { 
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Friendship' 
+        }
+    ]
 }, {
     // timestamps add 'created at' & 'updated at'
     timestamps: true
