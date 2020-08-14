a4570e058e4e95aaf47437d92a8f40c4e1deab8e
Daksh Khetan <2012daksh@gmail.com>
2020-08-14 19:28:52 +0530

    setup sockets

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
+// setup the chat server to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('Chat server is listening on port: 5000');
+
 // SASS middleware
 app.use(sassMiddleware({
     src: './assets/scss',
