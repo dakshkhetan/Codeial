c0fb5030b54986097db329225f6681cb9de15ff4
Daksh Khetan <2012daksh@gmail.com>
2020-08-15 19:59:52 +0530

    create initial sockets connection

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,3 +1,14 @@
 module.exports.chatSockets = function(socketServer){
 
+    let io = require('socket.io')(socketServer);
+
+    io.sockets.on('connection', function(socket){
+        console.log('new connection received:', socket.id);
+
+        socket.on('disconnect', function(){
+            console.log('socket disconnected!');
+        });
+
+    });
+
 }
