5c9b12f70abe06d3e6f0a8dd475762637ef5c471
Daksh Khetan <2012daksh@gmail.com>
2020-08-17 22:53:33 +0530

    implement chat box functionality

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -27,5 +27,42 @@ class ChatEngine {
           });
 
       });
+
+      // send a message on clicking the send message button
+      $('#send-message').click(function(){
+          let msg = $('#chat-message-input').val();
+
+          if(msg != ''){
+              self.socket.emit('send_message', {
+                  message: msg,
+                  user_email: self.userEmail,
+                  chatroom: 'codeial'
+              });
+          }
+      });
+
+      self.socket.on('receive_message', function(data){
+          console.log('message received:', data.message);
+
+          let newMessage = $('<li>');
+
+          let messageType = 'other-message';
+
+          if(data.user_email == self.userEmail){
+              messageType = 'self-message';
+          }
+
+          newMessage.append($('<span>', {
+              'html': data.message
+          }));
+
+          newMessage.append($('<sub>', {
+              'html': data.user_email
+          }));
+
+          newMessage.addClass(messageType);
+
+          $('#chat-messages-list').append(newMessage);
+      });
   }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,15 +1,15 @@
 module.exports.chatSockets = function(socketServer){
 
-    let io = require('socket.io')(socketServer);
+  let io = require('socket.io')(socketServer);
 
-    io.sockets.on('connection', function(socket){
-        console.log('new connection received:', socket.id);
+  io.sockets.on('connection', function(socket){
+      console.log('new connection received:', socket.id);
 
-        socket.on('disconnect', function(){
-            console.log('socket disconnected!');
-        });
+      socket.on('disconnect', function(){
+          console.log('socket disconnected!');
+      });
 
-        socket.on('join_room', function(data){
+      socket.on('join_room', function(data){
           console.log('joining request received:', data);
 
           socket.join(data.chatroom);
@@ -17,6 +17,11 @@ module.exports.chatSockets = function(socketServer){
           io.in(data.chatroom).emit('user_joined', data);
       });
 
-    });
+      // detect 'send_message' and broadcast to everyone in the room
+      socket.on('send_message', function(data){
+          io.in(data.chatroom).emit('receive_message', data);
+      });
+
+  });
 
 }
